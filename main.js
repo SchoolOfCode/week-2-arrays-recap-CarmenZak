5c4144936e3015a4c78bef35f00821693ad5b89a
CarmenZak <186872558+CarmenZak@users.noreply.github.com>
2024-12-03 23:06:36 +0000

    Homework attempt no.1

---
+++ main.js
@@ -0,0 +1,45 @@
+
+// ### 1. Favorite Songs
+
+// - Tiny Dancer
+// - Running Up That Hill
+// - Don't Stop Moving
+// - Tragedy
+// - Crazy Horses
+
+const favouriteSongs =["Tiny Dancer", "Running Up That Hill", "Don't Stop Moving", "Tragedy", "Crazy Horses"]
+
+// ### 2. Profit From First Seven Days of the School of Code's Penny Doubling Pyramid Scheme 💰💰💰
+
+// - 1
+// - 2
+// - 4
+// - 8
+// - 16
+// - 32
+// - 64
+
+const pennyDoublingSchemeProfit = [1, 2, 4, 8, 16, 32, 64]
+
+// ### 3. Friends
+
+// - Name is Chris; special skill is changing lives.
+// - Name is Lizzie; special skill is being friendly.
+// - Name is Ben; special skill is code rapping.
+// - Name is Tao; special skill is hand raising.
+// - Name is Liz; special skill is crochet.
+// - Name is Patrick; special skill is Call of Duty.
+// - Name is Tim; special skill is analogies.
+// - Name is James; special skill is friendship.
+// - Name is Joseph; special skill is being positive.
+// - Name is Max; special skill is papier-mâché.
+
+const friends = 
+["Chris", "changing lives"];
+["Lizzie", "being friendly"];
+["Ben", "code rapping"];
+["Tao","hand raising"];
+["Liz", "crochet"];
+["Patrick", "Call of Duty"];
+["Tim", "analogies"];
+
